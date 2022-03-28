63e894a0ae6748434a9b162ad95db9eaf00e1269
Jocelyn Naarden <tzionone@gmail.com>
2022-03-28 19:10:01 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -11,6 +11,41 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   // iterate through the tutorial array
+//   for (let tutorialIndex in tutorials){
+//       const singleTutorial = tutorials[tutorialIndex]
+//       console.log(`Single Tutorial ${tutorialIndex}`, singleTutorial)
+//       // make array is individual
+//       let wordArr = singleTutorial.split(' ');
+//       // console.log("Old word", wordArr)
+//       let newWordArr = wordArr.map(word => word[0].toUpperCase() + word.slice(1))
+//       // console.log("New Words", newWordArr)
+//       // tutorials.pop()
+//       tutorials.push(newWordArr.join(' '))
+//       tutorials.shift()
+//   }
+//   console.log(tutorials)
+// return tutorials
+// }
+
 const titleCased = () => {
+  // iterate through the tutorial array
+  for (let tutorialIndex in tutorials) {
+    // set variable for single tutorial
+    const singleTutorial = tutorials[tutorialIndex]
+    // split singleTutorial into an array of words
+    let wordArr = singleTutorial.split(' ');
+    // Map over wordArr and capitalize each word
+    let newWordArr = wordArr.map(word => word[0].toUpperCase() + word.slice(1))
+    // join new array with capitalized words into the tutorials function at the end
+    tutorials.push(newWordArr.join(' '))
+  }
+  // delete the tutorials from the beginning
+  for (let i = 0; i < 10; i++) {
+    tutorials.shift()
+  }
   return tutorials
 }
+
+// titleCased()
